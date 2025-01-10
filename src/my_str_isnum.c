b9ffdf679c23521ec1ab3d4f253336aecb567e1d
Romanecauvez <romane.cauvez@epitech.eu>
2025-01-10 19:22:10 +0100

    [+] now handels negative values

--- src/my_str_isnum.c
+++ src/my_str_isnum.c
@@ -11,9 +11,13 @@ Verifies if every characters of a string are numeric
 */
 int my_str_isnum(char const *str)
 {
+    int i = 0;
+
     if (!str)
         return 1;
-    for (int i = 0; str[i]; i++) {
+    if (str[0] == '-')
+        i = 1;
+    for (; str[i]; i++) {
         if (str[i] < '0' || str[i] > '9')
             return 0;
     }
