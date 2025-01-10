12db445ca7c0c1bb55cd18577a4a2bb938ff01a8
Romanecauvez <romane.cauvez@epitech.eu>
2025-01-10 16:11:59 +0100

    [~] moved functions in right file

--- src/displays.c
+++ src/displays.c
@@ -7,6 +7,26 @@
 
 #include "window.h"
 
+int display_window(window_t *w, sfText *text, int second, sfClock *clock)
+{
+    sfRenderWindow_clear(w->win, sfBlack);
+    sfRenderWindow_drawSprite(w->win, w->bg, NULL);
+    sfRenderWindow_drawText(w->win, text, NULL);
+    for (int i = 0; w->corners[i]; i++)
+        w->corners[i]->nb_ac = 0;
+    w->corners = parse_in_corners(w, second);
+    second = while_window_open(w, clock);
+    return second;
+}
+
+void display_text(window_t *w, char *fps_str, char *second_str)
+{
+    sfText_setString(w->fps, fps_str);
+    sfRenderWindow_drawText(w->win, w->fps, NULL);
+    sfText_setString(w->seconds, second_str);
+    sfRenderWindow_drawText(w->win, w->seconds, NULL);
+}
+
 void display_boundaries(window_t *w)
 {
     for (int i = 0; w->all_to[i]; i++)
--- src/open_window.c
+++ src/open_window.c
@@ -96,14 +96,6 @@ static int convert_fps_seconds(int is_malloc, char **fps_str,
     return second;
 }
 
-void display_text(window_t *w, char *fps_str, char *second_str)
-{
-    sfText_setString(w->fps, fps_str);
-    sfRenderWindow_drawText(w->win, w->fps, NULL);
-    sfText_setString(w->seconds, second_str);
-    sfRenderWindow_drawText(w->win, w->seconds, NULL);
-}
-
 int clock_handeling(sfClock *clock, window_t *w)
 {
     static int fps = 0;
@@ -141,18 +133,6 @@ int while_window_open(window_t *w, sfClock *clock)
     }
     second = clock_handeling(clock, w);
     sfRenderWindow_display(w->win);
-    return second;
-}
-
-int display_window(window_t *w, sfText *text, int second, sfClock *clock)
-{
-    sfRenderWindow_clear(w->win, sfBlack);
-    sfRenderWindow_drawSprite(w->win, w->bg, NULL);
-    sfRenderWindow_drawText(w->win, text, NULL);
-    for (int i = 0; w->corners[i]; i++)
-        w->corners[i]->nb_ac = 0;
-    w->corners = parse_in_corners(w, second);
-    second = while_window_open(w, clock);
     return second;
 }
 
